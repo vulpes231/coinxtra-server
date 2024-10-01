572da4b72014b63dfd554049c3972444e2140243
Vulpes231 <whykayshot@gmail.com>
2024-10-01 07:01:01 +0100

    init

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const { connectDB } = require("./configs/connectDB");
+const { default: mongoose } = require("mongoose");
+const cookieParser = require("cookie-parser");
+const { reqLogger, errorLogger } = require("./middlewares/logger");
+const { corsOptions } = require("./configs/corsOptions");
+require("dotenv").config();
+const cors = require("cors");
+const { verifyJWT } = require("./middlewares/verifyJWT");
+const { allowedOrigins } = require("./configs/allowedOrigins");
+
+const app = express();
+
+// connectDB();
+const PORT = process.env.PORT || 3000;
+
+app.use(reqLogger);
+
+app.use(cors(corsOptions));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cookieParser());
+
+// user endpoints wihtout auth
+app.use("/signin", require("./routes/user/signin"));
+app.use("/signup", require("./routes/user/signup"));
+app.use("/", require("./routes/root"));
+
+// user endpoints with auth
+app.use(verifyJWT);
+
+app.use(errorLogger);
+
+// mongoose.connection.once("connected", () => {
+//   server.listen(PORT, () =>
+//     console.log(`Server started on http://localhost:${PORT}`)
+//   );
+// });
+
+app.listen(PORT, () =>
+  console.log(`Server started on http://localhost:${PORT}`)
+);
---
+++ middlewares/verifyJWT.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+
+const verifyJWT = (req, res, next) => {
+  const header = req.headers["authorization"];
+  if (!header)
+    return res.status(401).json({ message: "You are not logged in!" });
+
+  const token = header.split(" ")[1];
+  // console.log(`Bearer ${token}`);
+  if (!token) return res.status(403).json({ message: "Forbidden!" });
+
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+    if (err) return res.status(400).json({ message: "Bad token!" });
+    req.user = decoded.username;
+    req.userId = decoded.userId;
+
+    next();
+  });
+};
+
+module.exports = { verifyJWT };
