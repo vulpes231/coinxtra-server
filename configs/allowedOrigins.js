aad9427dd067fbbd79d5cc36a5eba6a1246d0db1
Vulpes231 <whykayshot@gmail.com>
2024-10-05 10:14:15 +0100

    cors

--- configs/allowedOrigins.js
+++ configs/allowedOrigins.js
@@ -1,3 +1,8 @@
-const allowedOrigins = ["http://localhost:5173", "http://localhost:6500"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:6500",
+  "https://coinxtra-server.onrender.com",
+  "https://coinxtra.netlify.app",
+];
 
 module.exports = { allowedOrigins };
